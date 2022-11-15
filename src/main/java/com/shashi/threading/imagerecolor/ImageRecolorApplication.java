c4b5f9dd33d7c6c6377930c6af29695fba0ecb54
Shashi Kumar <shashisingh752@gmail.com>
2022-11-15 18:51:20 +0530

    recoloring using single thread(main)

--- src/main/java/com/shashi/threading/imagerecolor/ImageRecolorApplication.java
+++ src/main/java/com/shashi/threading/imagerecolor/ImageRecolorApplication.java
@@ -1,13 +1,34 @@
 package com.shashi.threading.imagerecolor;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ImageRecolorApplication {
 
-	public static void main(String[] args) {
+	public static final String SOURCE_FILE =
+			"/C:/Users/shash/code/corejava/image-recolor/src/main/resources/flower.jpg";
+
+	public static final String DEST_FILE =
+			"/C:/Users/shash/code/corejava/image-recolor/src/main/resources/changed-color-flower.jpg";
+
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ImageRecolorApplication.class, args);
+
+		File file = new File(SOURCE_FILE);
+		BufferedImage srcImg = ImageIO.read(file);
+
+		int w = srcImg.getWidth();
+		int h = srcImg.getHeight();
+		BufferedImage destImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+
+		SimpleImageRecolor.changePixelColor(srcImg, destImg, w, h);
+
+		ImageIO.write(destImg, "jpg", new File(DEST_FILE));
 	}
 
 }
---
+++ src/main/java/com/shashi/threading/imagerecolor/SimpleImageRecolor.java
@@ -0,0 +1,58 @@
+package com.shashi.threading.imagerecolor;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class SimpleImageRecolor {
+
+    public static void changePixelColor(BufferedImage srcImg, BufferedImage destImg, int w, int h)
+            throws IOException {
+
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                int rgb = srcImg.getRGB(i, j);
+                if (isShadeOfGray(rgb)) {
+                    setPixelRgb(destImg, i, j, 0xFF0000FF);
+                } else {
+                    setPixelRgb(destImg, i, j, rgb);
+                }
+            }
+        }
+    }
+
+    private static void setPixelRgb(BufferedImage destImg, int i, int j, int rgb) {
+        destImg.getRaster().setDataElements(i, j,
+                destImg.getColorModel().getDataElements(rgb, null));
+    }
+
+    private static boolean isShadeOfGray(int rgb) {
+        int b = getColorBlue(rgb);
+        int g = getColorGrean(rgb);
+        int r = getColorRed(rgb);
+
+        return Math.abs(r - b) < 30 && Math.abs(r - g) < 30 && Math.abs(g - b) < 30;
+    }
+
+    public static int getColorBlue(int rgb) {
+        return rgb & 0x000000FF;
+    }
+
+    public static int getColorGrean(int rgb) {
+        return (rgb & 0x0000FF00) >> 8;
+    }
+
+    public static int getColorRed(int rgb) {
+        return (rgb & 0x00FF0000) >> 16;
+    }
+
+    public static int getRGB(int blue, int green, int red) {
+        int rgb = 0;
+
+        rgb |= blue;
+        rgb |= green << 8;
+        rgb |= red << 16;
+        rgb |= 0xFF000000; // assign the ful transparency value
+        return rgb;
+    }
+
+}
