a789472e74d645742e0e7b8e8460d8c1897bc016
Shashi Kumar <shashisingh752@gmail.com>
2022-11-15 15:54:32 +0530

    app skeleton

---
+++ src/main/java/com/shashi/threading/imagerecolor/ImageRecolorApplication.java
@@ -0,0 +1,13 @@
+package com.shashi.threading.imagerecolor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageRecolorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageRecolorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shashi/threading/imagerecolor/ImageRecolorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashi.threading.imagerecolor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageRecolorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
